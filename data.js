8cee86918d4402bb4f7e93ad12adc24d70e5edb1
Jakob Fix <jakob@jfix.com>
2016-02-27 20:12:50 +0100

    fixed wrong image id

--- data.js
+++ data.js
@@ -8,5 +8,5 @@ var myCards3 = [
 	{ "id": "7", "title": "citrus", "url_s": "https://farm2.staticflickr.com/1614/24570128545_54ec0d60e1_m_d.jpg" },
 	{ "id": "8", "title": "grape", "url_s": "https://farm2.staticflickr.com/1466/24543916806_04d4f8b284_m_d.jpg" },
 	{ "id": "9", "title": "kiwi", "url_s": "https://farm2.staticflickr.com/1567/24274488120_f79186e9ed_m_d.jpg" },
-	{ "id": "2", "title": "lettuce", "url_s": "https://farm2.staticflickr.com/1538/24570001415_73d4afcc04_m_d.jpg" }
+	{ "id": "10", "title": "lettuce", "url_s": "https://farm2.staticflickr.com/1538/24570001415_73d4afcc04_m_d.jpg" }
 ];
