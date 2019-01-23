d4e55dac26c79bc1778285b25a023675a9f4f8cc
Jakob Fix <jakob.fix@gmail.com>
2019-01-24 00:04:25 +0100

    Store images locally

---
+++ data-local.js
@@ -0,0 +1,12 @@
+var myCards3 = [
+	{ "id": "1", "title": "apple", "url_s": "assets/24462063152_e5e8feff2a_m.jpg" },
+	{ "id": "2", "title": "apricot", "url_s": "assets/23943524223_944a721720_m.jpg" },
+	{ "id": "3", "title": "asparagus", "url_s": "assets/23943517333_24c4d2a5ed_m.jpg" },
+	{ "id": "4", "title": "avocado", "url_s": "assets/23943507353_7900e86ced_m.jpg" },
+	{ "id": "5", "title": "brocoli", "url_s": "assets/24274752000_dc06629400_m.jpg" },
+	{ "id": "6", "title": "carot", "url_s": "assets/24487937331_ec86d9ed44_m.jpg" },
+	{ "id": "7", "title": "citrus", "url_s": "assets/24570128545_54ec0d60e1_m.jpg" },
+	{ "id": "8", "title": "grape", "url_s": "assets/24543916806_04d4f8b284_m.jpg" },
+	{ "id": "9", "title": "kiwi", "url_s": "assets/24274488120_f79186e9ed_m.jpg" },
+	{ "id": "10", "title": "lettuce", "url_s": "assets/24570001415_73d4afcc04_m.jpg" }
+];
